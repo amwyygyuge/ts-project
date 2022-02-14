c2860c348707888825088aae4486fb4b8610b19b
arthur.cai <arthur.cai@ringcentral.com>
2022-02-14 18:43:57 +0800

    feat(lazy): lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Test } from "./components";
 
 interface IProps {
   name: string;
 }
 
+const LazyComponent = lazy(
+  () => import("./components/LazyComponent/LazyComponent")
+);
 function App(props: IProps) {
   const { name } = props;
   return (
     <div className="app">
       <Test />
       <span>{`Hello!dadwad dsdasIddd'm ${name},  dddyears old.`}</span>
+      <Suspense fallback={<div>loding</div>}>
+        <LazyComponent />
+      </Suspense>
     </div>
   );
 }
---
+++ src/components/LazyComponent/LazyComponent.tsx
@@ -0,0 +1,12 @@
+/*
+ * @Author: Arthur Cai (arthur.cai@ringcentral.com)
+ * @Date: 2022-02-13 22:03:04
+ * @Last Modified by: Arthur Cai (arthur.cai@ringcentral.com)
+ * @Last Modified time: 2022-02-14 18:39:42
+ */
+import React from "react";
+import "./LazyComponent.css";
+
+export default function LazyComponent() {
+  return <div id="lazy">LazyComponent</div>;
+}
